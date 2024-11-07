cafa6b3b994e304d27be357771290b42b6169f9d
amine-ipssi-25 <a.mahi@ecole-ipssi.net>
2024-11-07 23:38:38 +0100

    Post brut

--- src/components/02-home/home.js
+++ src/components/02-home/home.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Home = () => {
-    const [articles, setArticles] = useState({})
+    const [articles, setArticles] = useState([])
 
     useEffect(() => {
         const fetchbyUser = async() => {
           try {
             const {data} = await axios.get(" http://localhost:8000/api/article/get")
             setArticles(data)
+            console.log(data)
             
           } catch (error) {
             console.error(error.message);
@@ -19,7 +20,12 @@ const Home = () => {
   return (
     <div>
         <h1>Home Page</h1>
-        <p>{articles.name}</p>
+        {articles.map((product, index) =>(
+            <div key={index}>
+            <p>{product.name}</p>
+            <img src={product.picture} width={50} alt="picture" />
+          </div>
+        ) )}
     </div>
   )
 }
