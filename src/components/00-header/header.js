e85c63ea058cbda8362faea1260d1643bfe02381
amine-ipssi-25 <a.mahi@ecole-ipssi.net>
2024-11-08 12:54:58 +0100

    Finish

--- src/components/00-header/header.js
+++ src/components/00-header/header.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
+import './index.css'
 
 import { Link, Outlet } from "react-router-dom";
 const Header = () => {
-  const [id, setId] = useState(25);
   return (
     <main>
       <header>
